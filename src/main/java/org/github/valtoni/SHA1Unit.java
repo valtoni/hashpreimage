fcde674807261ced7d987f9680c87803e677b059
Valtoni Boaventura <valtoni@gmail.com>
2024-05-12 08:06:28 -0400

    MessageDigest to constructor in SHA1Unit

--- src/main/java/org/github/valtoni/SHA1Unit.java
+++ src/main/java/org/github/valtoni/SHA1Unit.java
@@ -7,40 +7,40 @@ import java.util.Random;
 
 public class SHA1Unit {
 
-    private MessageDigest sha1;
-        private final byte[] input;
-        private final byte[] digest;
+    private final byte[] input;
+    private final byte[] digest;
 
-        public SHA1Unit() {
-            try {
-                if (sha1 == null) sha1 = MessageDigest.getInstance("SHA-1");
-            } catch (NoSuchAlgorithmException e) {
-                throw new RuntimeException(e);
-            }
-            this.input = sha1.digest(new byte[64]);
-            Random random = new Random();
-            random.nextBytes(input);
-            this.digest = sha1.digest(input);
+    public SHA1Unit() {
+        MessageDigest sha1;
+        try {
+            sha1 = MessageDigest.getInstance("SHA-1");
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException(e);
         }
-
-        private String bytesToHex(byte[] bytes) {
-            StringBuilder sb = new StringBuilder();
-            for (byte b : bytes) {
-                sb.append(String.format("%02x", b));
-            }
-            return sb.toString();
-        }
-
-        public String hexDigest() {
-            return bytesToHex(digest);
-        }
-
-        public String hexInput() {
-            return bytesToHex(input);
-        }
-
-        public boolean equals(SHA1Unit other) {
-            return Arrays.equals(digest, other.digest);
-        }
-
+        this.input = sha1.digest(new byte[64]);
+        Random random = new Random();
+        random.nextBytes(input);
+        this.digest = sha1.digest(input);
     }
+
+    private String bytesToHex(byte[] bytes) {
+        StringBuilder sb = new StringBuilder();
+        for (byte b : bytes) {
+            sb.append(String.format("%02x", b));
+        }
+        return sb.toString();
+    }
+
+    public String hexDigest() {
+        return bytesToHex(digest);
+    }
+
+    public String hexInput() {
+        return bytesToHex(input);
+    }
+
+    public boolean equals(SHA1Unit other) {
+        return Arrays.equals(digest, other.digest);
+    }
+
+}
